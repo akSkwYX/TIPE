346818262a978f9bf5ed9159de1bf53395d4526d
Eliott Hurot <131192436+akSkwYX@users.noreply.github.com>
2024-06-04 13:40:54 +0000

    chore: Update ModelTempHouse.c to improve calculation of total_cond and total_conv in updateIntAirs function

--- ModelTempHouse.c
+++ ModelTempHouse.c
@@ -23,12 +23,11 @@ struct cell
 {
     float T; // en degré Celsius
     char* type;
-	coord co;
-	float CTherVol;
-	float lambda; //conductivité thermique en W.K-1.m-1
-	float surface; // en m2
-	float epaisseur; // en m
-	float conductivite; // en W.K-1.
+	 coord co;
+	 float CTherVol;
+	 float lambda; //conductivité thermique en W.K-1.m-1
+	 float surface; // en m2
+	 float epaisseur; // en m
 };
 
 typedef struct cell cell;
@@ -43,7 +42,6 @@ struct structure
 	float lambda;
 	float surface;
 	float epaisseur;
-	float conductivite;
 };
 
 typedef struct structure structure;
@@ -90,26 +88,20 @@ void add_list(list *l, int i, int j)
 
 void updateWalls(cell **map, list *walls)
 {
-	int toAdd[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Coordonnées à ajouter aux coordonnées de la cellule regardée pour parcourir les cellules adjacentes
     for (int k = 0; k < walls->taille; k++)
     {
-		cell actCell = map[walls->t[k].i][walls->t[k].j];
-		float flux_tot = 0;
-        for (int l = 0; l<4; l++)
-		{
-			cell c = map[actCell.co.i + toAdd[l][0]][actCell.co.j + toAdd[l][1]];
-            if (strcmp(c.type, "airExt") == 0)
+        for (int x = -1; x < 2; x++)
+        {
+            for (int y = -1; y < 2; y++)
             {
-                flux_tot += c.conductivite * actCell.surface * (actCell.T - c.T);
-				map[walls->t[k].i + toAdd[l][0]][walls->t[k].j + toAdd[l][1]].T += (1/(actCell.conductivite * actCell.surface))*(c.conductivite * actCell.surface * (actCell.T - c.T));
+                if (strcmp(map[walls->t[k].i + x][walls->t[k].j + y].type, "airExt") == 0)
+                {
+                    map[walls->t[k].i][walls->t[k].j].T = map[walls->t[k].i + x][walls->t[k].j + y].T;
+                    goto end;
+                }
             }
-			else if (strcmp(c.type, "airInt") == 0)
-			{
-				flux_tot += c.conductivite * actCell.surface * (actCell.T - c.T);
-				map[walls->t[k].i + toAdd[l][0]][walls->t[k].j + toAdd[l][1]].T += (1/(actCell.conductivite * actCell.surface))*(c.conductivite * actCell.surface * (actCell.T - c.T));
-			}
         }
-		map[walls->t[k].i][walls->t[k].j].T -= (1/(actCell.conductivite * actCell.surface))*flux_tot;
+    end:;
     }
 }
 
@@ -121,41 +113,46 @@ void updateIntAirs(cell **map, list* intAirs){
 	for (int k = 0; k < intAirs->taille; k++)
 	{
 		cell actCell = map[intAirs->t[k].i][intAirs->t[k].j];
-		float act_rth = actCell.epaisseur / (actCell.lambda * actCell.surface);
 		float total_cond = 0;
         float total_conv = 0;
-		float total_chal = 0;
+		float total_chaleur = 0;
 		for (int l = 0; l < 4; l++){
 			cell c = map[actCell.co.i + toAdd[l][0]][actCell.co.j + toAdd[l][1]];
-			float c_rth = c.epaisseur / (c.lambda * c.surface);
-			total_cond += (c.T / (actCell.CTherVol * actCell.epaisseur * actCell.surface * act_rth)) * 1 - (actCell.T / actCell.CTherVol * actCell.epaisseur * actCell.surface) * 1;
+			total_cond += ((c.T - actCell.T) * 1 * c.lambda * c.surface) / ((actCell.CTherVol * actCell.surface * actCell.epaisseur) * c.epaisseur);
         	total_conv += c.T - actCell.T;
+			//if (strcmp(c.type, "radiateur")==0){
+			//	total_chaleur += (3500*1)/(actCell.CTherVol * actCell.surface * actCell.epaisseur);
+			//}
         }
-		map[intAirs->t[k].i][intAirs->t[k].j].T += total_cond + total_conv/(4*150);
+		map[intAirs->t[k].i][intAirs->t[k].j].T += total_cond + (total_conv/(4*300));
 	}
 }
 
 // Mise à jour de l'air extérieur
 
-void updateExtAirs(cell **map, list *extAirs, int height, int width, float Text){
-	for (int k = 0; k < extAirs->taille; k++)
-	{
-		map[extAirs->t[k].i][extAirs->t[k].j].T = Text;
-	}
+void updateExtAirs(cell **map, list *extAirs)
+{
 }
 
-// Mise à jour des structures
+// Mise à jour des fenêtres
 
 void updateStructures(cell **map, list *structures)
 {
-	int toAdd[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // Coordonnées à ajouter aux coordonnées de la cellule regardée pour parcourir les cellules adjacentes
     for (int k = 0; k < structures->taille; k++)
     {
-		cell actCell = map[structures->t[k].i][structures->t[k].j];
-		for (int l=0; l < 4; l++)
+        for (int x = -1; x < 2; x++)
         {
-		}
-	}
+            for (int y = -1; y < 2; y++)
+            {
+            if (strcmp(map[structures->t[k].i + x][structures->t[k].j + y].type, "airExt") == 0)
+                {
+                    map[structures->t[k].i][structures->t[k].j].T = map[structures->t[k].i + x][structures->t[k].j + y].T;
+                    goto end;
+                }
+            }
+        }
+    end:;
+    }
 }
 
 
@@ -187,6 +184,10 @@ void printMap(cell **map, int height, int width)
 			{
 				printf("\x1b[43m");
 			}
+			else if (strcmp(map[i][j].type, "radiateur") == 0)
+			{
+				printf("\x1b[41m");
+			}
             printf("%.1f ", map[i][j].T);
             printf("\x1b[0m");
         }
@@ -197,18 +198,18 @@ void printMap(cell **map, int height, int width)
 
 /* Appelle toutes les fonctions de mise à jours */
 
-void nextStep(cell **map, list** lists, int height, int width, float Text)
+void nextStep(cell **map, list** lists)
 {
+	updateWalls(map, lists[0]);
 	updateStructures(map, lists[3]);
 	updateIntAirs(map, lists[1]);
-	updateWalls(map, lists[0]);
-	updateExtAirs(map, lists[2], height, width, Text);
+	updateExtAirs(map, lists[2]);
 }
 
 
 /* Initialise la carte des cellules avec leur différents paramètres */
 
-cell** initialize(int height, int width, int* house, structure* structures, int nbrStructure, float* temps, float* CTherVol, float* lambdas, float* surfaces, float* epaisseurs, float* conductivites, list** lists)
+cell** initialize(int height, int width, int* house, structure* structures, int nbrStructure, float* temps, float* CTherVol, float* lambdas, float* surfaces, float* epaisseurs, list**	lists)
 {
     cell** map = ( cell** )malloc( sizeof(cell*)*(height) );
     for (int i = 0; i < height; i++)
@@ -227,7 +228,6 @@ cell** initialize(int height, int width, int* house, structure* structures, int 
 					new[j].lambda = structures[k].lambda;
 					new[j].surface = structures[k].surface;
 					new[j].epaisseur = structures[k].epaisseur;
-					new[j].conductivite = structures[k].conductivite;
          			add_list(lists[3], i, j);
 					found = true;
 				}
@@ -243,7 +243,6 @@ cell** initialize(int height, int width, int* house, structure* structures, int 
 				new[j].lambda = lambdas[0];
 				new[j].surface = surfaces[0];
 				new[j].epaisseur = epaisseurs[0];
-				new[j].conductivite = conductivites[0];
             	add_list(lists[0], i, j);
 			}
             else if (i > house[0] && i < house[2] && j < house[3] && j > house[1])
@@ -256,7 +255,6 @@ cell** initialize(int height, int width, int* house, structure* structures, int 
 				new[j].lambda = lambdas[1];
 				new[j].surface = surfaces[1];
 				new[j].epaisseur = epaisseurs[1];
-				new[j].conductivite = conductivites[1];
             	add_list(lists[1], i, j);
             }
             else
@@ -269,7 +267,6 @@ cell** initialize(int height, int width, int* house, structure* structures, int 
 				new[j].lambda = lambdas[2];
 				new[j].surface = surfaces[2];
 				new[j].epaisseur = epaisseurs[2];
-				new[j].conductivite = conductivites[2];
                 add_list(lists[2], i, j);
             }
         }
@@ -284,8 +281,9 @@ int main()
    	int height = 18;
    	int width = 18;
 	int house[4] = {/* Corner top i */ 2, /* Corner top j */ 2, /* Corner bot i */ 15, /* Corner bot j */ 15};
+    int nbrStructure = 4;
 	structure window1 = {
-		.T = 15.0,
+		.T = 20.0,
 		.type = "window",
 		.begining = {.i = 5, .j = 2},
 		.ending = {.i = 7, .j = 2},
@@ -293,10 +291,9 @@ int main()
 		.lambda = 1.0,
 		.surface = 1,
 		.epaisseur = 0.1,
-		.conductivite = 4.5,
 	};
 	structure window2 = {
-		.T = 15.0,
+		.T = 20.0,
 		.type = "window",
 		.begining = {.i = 2, .j = 7},
 		.ending = {.i = 2, .j = 11},
@@ -304,10 +301,9 @@ int main()
 		.lambda = 1.0,
 		.surface = 1,
 		.epaisseur = 0.1,
-		.conductivite = 4.5,
 	};
 	structure door = {
-		.T = 15.0,
+		.T = 20.0,
 		.type = "door",
 		.begining = {.i = 15, .j = 7},
 		.ending = {.i = 15, .j = 7},
@@ -315,72 +311,42 @@ int main()
 		.lambda = 0.12,
 		.surface = 1,
 		.epaisseur = 0.1,
-		.conductivite = 1.0,
 	};
-	structure wall1 = {
-		.T = 15.0,
-		.type = "wall",
-		.begining = {.i = 10, .j = 6},
-		.ending = {.i = 14, .j = 6},
-		.CTherVol = 2400000,
-		.lambda = 0.061,
+	/*structure radiateur = {
+		.T = 20.0,
+		.type = "radiateur",
+		.begining = {.i = 5, .j = 14},
+		.ending = {.i = 6, .j = 14},
+		.CTherVol = 3200000,
+		.lambda = 55,
 		.surface = 1,
-		.epaisseur = 0.394,
-		.conductivite = 4.4,
-	};
-	structure wall2 = {
-		.T = 15.0,
-		.type = "wall",
-		.begining = {.i = 10, .j = 3},
-		.ending = {.i = 10, .j = 4},
-		.CTherVol = 2400000,
-		.lambda = 0.061,
-		.surface = 1,
-		.epaisseur = 0.394,
-		.conductivite = 4.4,
-	};
-	structure door_bedroom = {
-		.T = 15.0,
-		.type = "door",
-		.begining = {.i = 10, .j = 5},
-		.ending = {.i = 10, .j = 5},
-		.CTherVol = 350000,
-		.lambda = 0.12,
-		.surface = 0.5,
-		.epaisseur = 0.1,
-		.conductivite = 1.0,
-	};
-    int nbrStructure = 6;
-	structure init_structure[6] = {window1, window2, door, wall1, wall2, door_bedroom};
-	float temps[3] = {/* wall */ 20.0, /* AirInt */ 25.0, /* AirExt */ 15.0};
-	float CTherVol[3] = {/* wall */ 2400000, /* AirInt */ 1256, /* AirExt */ 1256};
-	float lambdas[3] = {/* wall */ 0.061 , /* AirInt */ 0.025 , /* AirExt */ 0.025};
-	float surfaces[3] = {/* wall */ 1 , /* AirInt */ 1 , /* AirExt */ 1};
-	float epaisseurs[3] = {/* wall */ 0.394 , /* AirInt */ 1 , /* AirExt */ 1};
-	float conductivites[3] = {/* wall */ 4.4 , /* AirInt */ 1 , /* AirExt */ 1};
+		.epaisseur = 0.2,
+	};*/
+	structure init_structure[3] = {window1, window2, door};
+	float temps[4] = {/* wall */ 25.0, /* AirInt */ 16.0, /* AirExt */ 25.0, /* window */ 25.0};
+	float CTherVol[4] = {/* wall */ 2400000, /* AirInt */ 1256, /* AirExt */ 1256, /* window */ 17000};
+	float lambdas[4] = {/* wall */ 0.061 , /* AirInt */ 0.025 , /* AirExt */ 0.025, /* window */ 1.0};
+	float surfaces[4] = {/* wall */ 1 , /* AirInt */ 1 , /* AirExt */ 1, /* window */ 1};
+	float epaisseurs[4] = {/* wall */ 0.394 , /* AirInt */ 1 , /* AirExt */ 1, /* window */ 0.1};
     int lapsTime = 86400;
     list* walls = create_list(height * width);
     list* intAirs = create_list(height * width);
     list* extAirs = create_list(height * width);
     list* structures = create_list(height * width);
 	list* lists[4] = {walls, intAirs, extAirs, structures};
-    cell** map = initialize(height, width, house, init_structure, nbrStructure, temps, CTherVol, lambdas, surfaces, epaisseurs, conductivites, lists);
+    cell** map = initialize(height, width, house, init_structure, nbrStructure, temps, CTherVol, lambdas, surfaces, epaisseurs, lists);
 	FILE* file = fopen("data.csv", "w");
     for (int t = 0; t <= lapsTime; t++)
     {
         fprintf(file, "%f,", map[house[0]+2][house[1]+1].T);
-        nextStep(map, lists, height, width, temps[2]);
+        nextStep(map, lists);
         if (t % 3600 == 0)
         {
             printf("%dh :\n", t / 3600);
             printMap(map, height, width);
             printf("\n");
         }
-		if (t < 3600 && t % 60 == 0){
-			printf("%dmin :\n", t / 60);
-			printMap(map, height, width);
-			printf("\n");
-		}
+        
     }
     return 0;
 }
